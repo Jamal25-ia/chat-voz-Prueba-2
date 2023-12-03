3c172b15de6a63e7ab797033429644e505f35e0e
Jamal25-ia <76519029+Jamal25-ia@users.noreply.github.com>
2023-12-03 16:27:45 -0400

    APartir de aca son cambios mios y de chatgpt

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
 from flask import Flask, render_template, request, redirect
 import speech_recognition as sr
+import base64
+import io
 
 app = Flask(__name__)
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     transcript = ""
     if request.method == "POST":
-        print("FORM DATA RECEIVED")
-
-        if "file" not in request.files:
+        if "audioData" not in request.form:
             return redirect(request.url)
 
-        file = request.files["file"]
-        if file.filename == "":
-            return redirect(request.url)
+        audio_data_url = request.form["audioData"]
+        audio_data = base64.b64decode(audio_data_url.split(',')[1])
 
-        if file:
-            recognizer = sr.Recognizer()
-            audioFile = sr.AudioFile(file)
-            with audioFile as source:
-                data = recognizer.record(source)
-            transcript = recognizer.recognize_google(data, key=None)
+        recognizer = sr.Recognizer()
+        audioFile = io.BytesIO(audio_data)
+        
+        with sr.AudioFile(audioFile) as source:
+            data = recognizer.record(source)
+
+        transcript = recognizer.recognize_google(data, key=None)
 
     return render_template('index.html', transcript=transcript)
-
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
